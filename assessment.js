df6455c22793426281ba9aa3e8e403937e15c76d
mitsuki321 <143688195+mitsuki321@users.noreply.github.com>
2023-09-01 17:15:06 +0900

    幅の調整

--- assessment.js
+++ assessment.js
@@ -14,6 +14,28 @@ assessmentButton.onclick = () => {
   console.log(userName)
   //診断結果表示
   resultDivision.innerText = '';
+  //headerDivisionの作成
+  const headerDivision = document.createElement('div');
+  headerDivision.setAttribute('class','card-header text-bg-primary');
+  headerDivision.innerText = '診断結果';
+
+  //bodyDivisionの作成
+  const bodyDivision = document.createElement('div');
+  bodyDivision.setAttribute ('class','card-body');
+
+  const paragraph = document.createElement('p');
+  paragraph.setAttribute ('class', 'card-text');
+  const result = assessment(userName);
+  paragraph.innerText = result
+  bodyDivision.appendChild(paragraph)
+
+  //
+  bodyDivision.setAttribute('class','card');
+
+  resultDivision.appendChild(headerDivision);
+  resultDivision.appendChild(bodyDivision);
+
+/*
   const header =document.createElement('h3');
   header.innerText ='診断結果';
   resultDivision.appendChild(header);
@@ -22,7 +44,8 @@ assessmentButton.onclick = () => {
   const result = assessment(userName);
   paragraph.innerText = result;
   resultDivision.appendChild(paragraph);
-  
+  */
+
   //ツイートエリア表示
   tweetDivision.innerText ='';
   const anchor = document.createElement('a');
